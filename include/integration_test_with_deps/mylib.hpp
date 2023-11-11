f0213d97ea80e186c19ecb7a8bee212621e1eb6c
Yannic Staudt <yannic@tipi.build>
2023-11-11 14:41:48 +0100

    :wrench: adding dependent project as submodule

--- include/integration_test_with_deps/mylib.hpp
+++ include/integration_test_with_deps/mylib.hpp
@@ -8,6 +8,6 @@ namespace tipi::integration_tests::project_with_deps {
     using namespace std::string_literals;
 
     inline std::string library_call() {
-        return "tipi-build/integration_test_project_with_deps@main::library_call()->"s + tipi::integration_tests::project_deep::library_call(); 
+        return "tipi-build/integration_test_project_with_deps@dep-as-submodule::library_call()->"s + tipi::integration_tests::project_deep::library_call(); 
     }
 }
